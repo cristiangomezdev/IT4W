daa833364caa3cb446370edbd0b556609dd4812d
cristiangomezdev <cristiangomez.dev@gmail.com>
2026-03-10 14:59:47 -0300

    .

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -1,7 +1,6 @@
 import Character from "./Character";
 
 const Characters = ({ characters }) => {
-
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="p-8">
